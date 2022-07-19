5155505a07be93b9f693547583c0a0f4ca30ed3f
alexisxz <alexisxzinho@hotmail.com>
2022-07-19 14:08:29 +0200

    final project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import statusRoute from "./routes/status.route";
+import usersRoute from "./routes/users.route";
 
 const app = express();
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'sucesso total! vocês são demais!!' });
-});
+// configuração da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// configuração de rotas
+app.use(usersRoute);
+app.use(statusRoute)
+
+// inicialização do servidor
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000!');
 });
