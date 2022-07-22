bb3211aeab0574aaf0ec792cd7d42d193167db81
alexisxz <alexisxzinho@hotmail.com>
2022-07-22 13:01:44 +0200

    hiding the db

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString = 'postgres://tvlwmckb:YoNVcJvO_XdiueH6F9-8MPYIvVEERUVQ@motty.db.elephantsql.com/tvlwmckb';
+const connectionString = 'paste the db';
 
 const db = new Pool({ connectionString });
 
